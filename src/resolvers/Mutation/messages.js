17d84e4bfe3aa78de19dddd5e3f508f20c419b08
Kaden Barlow <>
2018-02-23 15:26:14 -0700

    Mutation for addingMessage (will find the conversation or create a new one) is go

--- src/resolvers/Mutation/messages.js
+++ src/resolvers/Mutation/messages.js
@@ -5,20 +5,38 @@ const messages = {
     // Authorize the user and query it to add the message to
     const user = await ctx.db.query.user({ where: { id: getUserId(ctx) } })
     // See if the user already has a conversation
-    const conversation = await ctx.db.query.conversations( {where: {threadId: args.threadId}})
+    let conversation = await ctx.db.query.conversations( {where: {threadId: args.threadId, user: {id: user.id}}})
+    console.log(conversation)
     // if not create a new conversation for the user
-    if(!conversation){ const conversation = await ctx.db.mutation.createConversation({data:{threadId: args.threadId, user: user}}) }
-    const message = await ctx.db.mutation.createMessage( {data: {
-      address: args.address,
-      body: args.body,
-      read: args.read,
-      error: args.error,
-      date: args.date,
-      threadId: args.threadId,
-      conversation: conversation
-    }})
+    if(conversation.length == 0) { 
+      conversation = await ctx.db.mutation.createConversation( {
+        data: {
+          threadId: args.threadId, 
+          user: {
+            connect: {
+              id: user.id
+            }
+          } 
+        }
+      }) 
+    }
+    else conversation = conversation[0]
+    const message = await ctx.db.mutation.createMessage( {
+      data: {
+        address: args.address,
+        body: args.body,
+        read: args.read,
+        error: args.error,
+        date: args.date,
+        threadId: args.threadId,
+        conversation: {
+          connect: {
+            id: conversation.id
+          }
+        }
+      }
+    })
     return message
-    //throw new Error(`Function not implemented yet`)
   },
 
   async updateMessage(parent, args, ctx, info) {
